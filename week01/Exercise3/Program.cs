656d46c608ba8d83b08d06bff8cc3a752c1fabb4
Heaven <gabrielheavenlydawn@gmail.com>
2025-03-07 19:17:49 +0100

    chore: Week one Exercise three

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -2,8 +2,46 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Random random = new Random();
+        string playAgain;
+
+        do
+        {
+            // Generating a random number from 1 to 100
+            int magicNumber = random.Next(1, 101); 
+            int guess;
+            int attempts = 0;
+            
+            Console.WriteLine("I have picked a magic number between 1 and 100. Try to guess it!");
+            
+            do
+            {
+                Console.Write("What is your guess? ");
+                guess = int.Parse(Console.ReadLine());
+                attempts++;
+                
+                if (guess < magicNumber)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else if (guess > magicNumber)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else
+                {
+                    Console.WriteLine($"You guessed it in {attempts} attempts!");
+                }
+            } while (guess != magicNumber);
+            
+            Console.Write("Do you want to play again? (yes/no): ");
+
+            playAgain = Console.ReadLine().ToLower();
+
+        } while (playAgain == "yes");
+        
+        Console.WriteLine("Thanks for playing! See Yah.");
     }
 }
